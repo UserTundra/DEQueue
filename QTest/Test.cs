aec64dd96e898bad53344afc0d381479fa7ec5f2
UserTundra <haizbq@gmail.com>
2018-05-13 15:40:01 +0400

    initial commit

---
+++ QTest/Test.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Queue;
+
+namespace QTest
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            DEQueue<string> deq = new DEQueue<string>();
+            Assert.IsTrue(deq.Size == 0);
+        }
+
+        [TestMethod]
+        public void PushFrontTest()
+        {
+            DEQueue<string> deq = new DEQueue<string>();
+            deq.pushFront("a");
+            Assert.IsTrue(deq.Size == 1);
+            
+            foreach (var item in deq)
+            {
+                Assert.AreEqual("a", item);
+            }
+        }
+
+        [TestMethod] 
+        public void PushFrontManyTest()
+        {
+            DEQueue<string> deq = new DEQueue<string>();
+            deq.pushFront("a");
+            deq.pushFront("b");
+            deq.pushFront("c");
+            Assert.IsTrue(deq.Size == 3);
+            Assert.IsTrue(deq.Head.Data == "c");
+            Assert.IsTrue(deq.Tail.Data == "a");
+        }
+
+        [TestMethod]
+        public void PushBack()
+        {
+            DEQueue<string> deq = new DEQueue<string>();
+            deq.pushBack("a");
+            Assert.IsTrue(deq.Size == 1);
+
+            foreach (var item in deq)
+            {
+                Assert.AreEqual("a", item);
+            }
+        }
+
+        [TestMethod]
+        public void PushBacktManyTest()
+        {
+            DEQueue<string> deq = new DEQueue<string>();
+            deq.pushBack("a");
+            deq.pushBack("b");
+            deq.pushBack("c");
+            Assert.IsTrue(deq.Size == 3);
+            Assert.IsTrue(deq.Head.Data == "a");
+            Assert.IsTrue(deq.Tail.Data == "c");
+        }
+
+        [TestMethod]
+        public void PopFrontTest()
+        {
+            DEQueue<string> deq = new DEQueue<string>();
+            deq.pushBack("a");
+            deq.pushBack("b");
+            deq.pushBack("c");
+            Assert.IsTrue(deq.Size == 3);
+            Assert.AreEqual("a", deq.Head.Data);
+            Assert.AreEqual("c", deq.Tail.Data);
+
+            deq.popFront();
+            Assert.IsTrue(deq.Size == 2);
+            Assert.AreEqual("b", deq.Head.Data);
+            Assert.AreEqual("c", deq.Tail.Data);
+
+            deq.popFront();
+            Assert.IsTrue(deq.Size == 1);
+            Assert.AreEqual("c", deq.Head.Data);
+            Assert.AreEqual("c", deq.Tail.Data);
+        }
+
+        [TestMethod]
+        public void PopFrontAloneTest()
+        {
+            DEQueue<string> deq = new DEQueue<string>();
+            deq.popFront();
+            Assert.IsTrue(deq.Size == 0);
+            Assert.IsNull(deq.Head);
+            Assert.IsNull(deq.Tail);
+        }
+
+        [TestMethod]
+        public void PopBackTest()
+        {
+            DEQueue<string> deq = new DEQueue<string>();
+            deq.pushBack("a");
+            deq.pushBack("b");
+            deq.pushBack("c");
+            Assert.IsTrue(deq.Size == 3);
+            Assert.AreEqual("a", deq.Head.Data);
+            Assert.AreEqual("c", deq.Tail.Data);
+
+            deq.popBack();
+            Assert.IsTrue(deq.Size == 2);
+            Assert.AreEqual("a", deq.Head.Data);
+            Assert.AreEqual("b", deq.Tail.Data);
+
+            deq.popBack();
+            Assert.IsTrue(deq.Size == 1);
+            Assert.AreEqual("a", deq.Head.Data);
+            Assert.AreEqual("a", deq.Tail.Data);
+        }
+
+        [TestMethod]
+        public void PopBackAloneTest()
+        {
+            DEQueue<string> deq = new DEQueue<string>();
+            deq.popBack();
+            Assert.IsTrue(deq.Size == 0);
+            Assert.IsNull(deq.Head);
+            Assert.IsNull(deq.Tail);
+        }
+
+        [TestMethod]
+        public void BackTest()
+        {
+            DEQueue<string> deq = new DEQueue<string>();
+            Assert.IsNull(deq.back());
+        }
+
+        [TestMethod]
+        public void BackManyTest()
+        {
+            DEQueue<string> deq = new DEQueue<string>();
+            deq.pushFront("a");
+            deq.pushBack("b");
+            deq.pushFront("c");
+            Assert.AreEqual("b", deq.back().Data);
+        }
+
+        [TestMethod]
+        public void FrontTest()
+        {
+            DEQueue<string> deq = new DEQueue<string>();
+            Assert.IsNull(deq.front());
+        }
+
+        [TestMethod]
+        public void FrontManyTest()
+        {
+            DEQueue<string> deq = new DEQueue<string>();
+            deq.pushFront("a");
+            deq.pushBack("b");
+            deq.pushFront("c");
+            Assert.AreEqual("c", deq.front().Data);
+        }
+
+        [TestMethod]
+        public void ClearTest()
+        {
+            DEQueue<string> deq = new DEQueue<string>();
+            deq.pushFront("a");
+            deq.pushBack("b");
+            deq.pushFront("c");
+            Assert.IsTrue(deq.Size == 3);
+            Assert.AreEqual("c", deq.Head.Data);
+            Assert.AreEqual("b", deq.Tail.Data);
+
+            deq.clear();
+            Assert.IsTrue(deq.Size == 0);
+            Assert.IsNull(deq.Head);
+            Assert.IsNull(deq.Tail);
+        }
+
+        [TestMethod]
+        public void ToArrayStringTest()
+        {
+            string[] output = { "c", "a", "b" };
+
+            DEQueue<string> deq = new DEQueue<string>();
+            deq.pushFront("a");
+            deq.pushBack("b");
+            deq.pushFront("c");
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.AreEqual(output[i], deq.toArray()[i]);
+            }
+        }
+
+        [TestMethod]
+        public void ToArrayIntTest()
+        {
+            int[] output = { 1, 2, 3, 4, 5 };
+            DEQueue<int> deq = new DEQueue<int>();
+            deq.pushFront(1);
+            deq.pushBack(2);
+            deq.pushBack(3);
+            deq.pushBack(4);
+            deq.pushBack(5);
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.AreEqual(output[i], deq.toArray()[i]);
+            }
+        }
+
+
+    }
+}
---
+++ Queue/DEQueue.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    public class DEQueue<T>  : IEnumerable<T>
+    {
+        
+        Node<T> head;
+        Node<T> tail;
+        int count;
+
+        public DEQueue()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+        public int Size
+        {
+            get { return count; }
+        }
+
+        public Node<T> Head
+        {
+            get { return head; }
+        }
+
+        public Node<T> Tail
+        {
+            get { return tail; }
+        }
+
+        public void pushBack(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (count == 0)
+                head = node;
+            else
+            {
+                tail.Next = node;
+            }
+            node.Previous = tail;
+            tail = node;
+            count++;
+
+        }
+
+        public void pushFront(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (count == 0)
+            {
+                head = node;
+                tail = head;
+            }
+            else
+            {
+                Node<T> buf = head;
+                node.Next = buf;
+                head = node;
+                buf.Previous = node;
+            }
+            count++;
+        }
+
+        public Node<T> popFront()
+        {
+            Node<T> pointer = head;
+            if (count != 0)
+            {
+                head = head.Next;
+                count--;
+            }
+            return pointer;
+        }
+
+        public Node<T> popBack()
+        {
+            Node<T> pointer = tail;
+            if (count != 0 )
+            {
+                tail = tail.Previous;
+                count--;
+            }
+            return pointer;
+        }
+
+        public Node<T> back()
+        {
+            return tail;
+        }
+
+        public Node<T> front()
+        {
+            return head;
+        }
+
+        public void clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public T[] toArray()
+        {
+            T[] array = new T[this.Size];
+            int idx = 0;
+            foreach (var el in this)
+            {
+                array[idx++] = el;
+            }
+            return array;
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
